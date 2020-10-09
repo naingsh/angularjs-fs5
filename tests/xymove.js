cd91ab16adceadde27fd87d6304ef59653ef22ab
Pine <ns.h32ab@gmail.com>
2020-10-09 15:30:50 +0630

    some xy movements implementation

--- tests/xymove.js
+++ tests/xymove.js
@@ -1,18 +1,54 @@
 (function(){
 
-angular.module('xy', []).controller('xyCtrl',move);
 
+angular.module('xy', []).controller('xyCtrl',move);
+var count={
+  south:0,
+  east:0
+};
 function move($scope){
   $scope.key="";
   $scope.checkKey=function(){
     var ch=$scope.key.charAt($scope.key.length-1);
       switch(ch){
-        case 'w': console.log("north");break;
-        case 'a': console.log("west");break;
-        case 's': console.log("south");break;
-        case 'd': console.log("east");break;
+        case 'w': console.log("north");
+                  north();
+                  break;
+        case 'a': console.log("west");
+                  west();
+                  break;
+        case 's': console.log("south");
+                  south();
+                  break;
+        case 'd': console.log("east");
+                  east();
+                  break;
+        case '0': var elt=document.querySelector('#car');
+                  elt.style.left=0;
+                  elt.style.top=0;
         default: console.log("stop");break;
       }
   };
+}
+
+function east(){
+  var elt=document.querySelector("#car");
+  count.east++;
+  elt.style.left=count.east+"px";
+}
+function north(){
+  var elt=document.querySelector("#car");
+  count.south--;
+  elt.style.top=count.south+"px";
+}
+function west(){
+  var elt=document.querySelector("#car");
+  count.east--;
+  elt.style.left=count.east+"px";
+}
+function south(){
+  var elt=document.querySelector("#car");
+  count.south++;
+  elt.style.top=count.south+"px";
 }
 })();
