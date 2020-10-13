ad7a10c463c1eb632d3fc661afb5dd48e84615d6
Pine <ns.h32ab@gmail.com>
2020-10-13 19:33:17 +0630

    lecture13

---
+++ examples/lecture13/app.js
@@ -0,0 +1,81 @@
+  (function(){
+
+    angular.module('feedApp',[])
+    .controller('feedCtrl',stateOfKirby)
+    .filter('loves', LoveFilterFactory)
+    .filter('fruit', fruitFilterFactory)
+    .filter('truth', TruthFilter);
+
+  stateOfKirby.$inject = ['$scope','lovesFilter','fruitFilter','truthFilter'];
+  function stateOfKirby($scope,lovesFilter,fruitFilter,truthFilter){
+  $scope.state = "hungry";
+  $scope.response = "";
+  $scope.feedKirby = function(){
+    $scope.state = "fed";
+    $scope.response = "";
+  };
+  $scope.sayMsg = function(){
+    var msg = "Kirby likes watermelon";
+    return msg;
+  };
+
+  $scope.sayFilteredMsg = function(){
+    var msg = "Kirby likes watermelon";
+    msg = lovesFilter(msg);
+    return msg;
+  };
+
+var extMsg = "Kirby likes watermelon";
+  $scope.replacedFruitMsg = function(){
+    var msg = "Kirby likes watermelon";
+    msg = fruitFilter(msg);
+    return msg;
+  };
+
+  $scope.doubleFilteredMsg = function(){
+    extMsg = lovesFilter(extMsg); //msg is already filtered in above "replacedFruitMsg" function
+    extMsg = fruitFilter(extMsg);
+    return extMsg;
+  };
+
+  $scope.truthBtn = function(){
+    $scope.truthMsg = "Kirby likes watermelon";
+    var target = "likes";
+    var replace = "is";
+    $scope.truthMsg = truthFilter($scope.truthMsg,target,replace);
+    $scope.truthMsg = truthFilter($scope.truthMsg,"watermelon","horny af");
+  };
+
+  $scope.truthCloseBtn = function(){
+    $scope.truthMsg = "";
+  };
+
+  $scope.wantsMore = function(){
+    $scope.state = "hungry";
+    $scope.response = "yes!!";
+  };
+
+  }
+
+    function LoveFilterFactory(){
+      return function(input){
+        input = input || "";  //if there's nth in "input" make it empty variable
+        input = input.replace("likes", "loves");  //replace likes with loves
+        return input;
+      };
+    }
+    function fruitFilterFactory(){
+      return function(input){
+        input = input || "";
+        input = input.replace("watermelon","banana");
+        return input;
+      };
+    }
+    function TruthFilter(){
+      return function(input,target,replace){
+        input = input || "";
+        input = input.replace(target,replace);
+        return input;
+      };
+    }
+  })();
