6d503e082c2b4421ed46f3989a569f52689d0ca5
Pine <ns.h32ab@gmail.com>
2020-10-15 11:03:03 +0630

    temp file added

---
+++ tests/temp/script.js
@@ -0,0 +1,34 @@
+var vowels =  {
+  a:'a',
+  e:'e',
+  i:'i',
+  o:'o',
+  u:'u'
+};
+function eat(thing){
+  var char = thing.charAt(0);
+  var bool = false;
+  for(var i in vowels){
+    if(char==i) {
+      bool=true;break;
+    }
+  }
+  switch(bool){
+    case true : console.log("Eating an "+thing);break;
+    case false : console.log("Eating a "+thing);break;
+    default : break;
+  }
+}
+
+// eat("ice-cream");
+
+// nuts();
+// function nuts(peanut){
+//   var hazelnut = "hazelnut";
+//   eat(almond);
+//   function seeds(){
+//       var almond = "almond";
+//       eat(hazelnut);
+//   }
+// seeds();
+// }
