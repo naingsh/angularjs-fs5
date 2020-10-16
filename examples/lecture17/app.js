aeaf68e6ad3f9caaf5d2082a6f72316aa7236d13
Pine <ns.h32ab@gmail.com>
2020-10-16 20:36:32 +0630

    added shopping list

---
+++ examples/lecture17/app.js
@@ -0,0 +1,45 @@
+var shoppingList1 = ["Ballon","Nail File","Hair Tie","Spoon","CD","Doll",
+"Eyeliner","Picture Frame","Sun Glasses","Greeting Card"];
+
+var shoppingList2 = [
+    {
+      name: "Ballon",
+      quantity: 1000
+    },
+    {
+      name: "CD",
+      quantity: 10
+    },
+    {
+      name: "Sun Glasses",
+      quantity: 2
+    },
+    {
+      name: "Picture Frame",
+      quantity: 69
+    },
+    {
+        name: "Orange",
+        quantity: 50
+    }
+];
+
+(function(){
+  angular.module('shoppingApp', []).controller('shoppingCtrl', shoppingCtrl);
+  shoppingCtrl.$inject = ['$scope'];
+  function shoppingCtrl($scope){
+    $scope.list1 = shoppingList1;
+    $scope.list2 = shoppingList2;
+    $scope.logItems = function(){
+      console.log($scope.list2);
+    };
+    $scope.addItem = function(){
+      $scope.newItem = {
+        name: $scope.newItemName,
+        quantity: $scope.newItemQuantity
+      };
+      $scope.list2.push($scope.newItem);
+    };
+  }
+
+})();
