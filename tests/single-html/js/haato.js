d74f865e36936bc3f1c79915ded62565d86f5946
Pine <ns.h32ab@gmail.com>
2020-11-09 14:05:07 +0630

    haato

--- tests/single-html/js/haato.js
+++ tests/single-html/js/haato.js
@@ -3,27 +3,44 @@
   angular.module('haatoApp',[])
   .controller('haatoCtrl',haatoCtrl)
   .service('getGifService',getGifService)
-  .constant('haatoUrl','https://nshtut.github.io/angular-fs5/tests/single-html/snippets/hatoPanik.html')
-  .constant('hololiveEveryone',)
-  //https://github.com/nshtut/angular-fs5/blob/main/tests/single-html/media/hatoPanik.gif
-
-haatoCtrl.$inject = ['getGifService']
-  function haatoCtrl(getGifService){
+  // .constant('haatoUrl','https://nshtut.github.io/angular-fs5/tests/single-html/snippets/hatoPanik.html')
+  // .constant('hololiveEveryone','https://nshtut.github.io/angular-fs5/tests/single-html/snippets/everyone.html')
+  .constant('urls',['https://nshtut.github.io/angular-fs5/tests/single-html/snippets/everyone.html'
+  ,'https://nshtut.github.io/angular-fs5/tests/single-html/snippets/hatoPanik.html']);
+haatoCtrl.$inject = ['$timeout','getGifService','urls']
+  function haatoCtrl($timeout,getGifService,urls){
     var feet = this;
-    var promise = getGifService.getHaato();
-    promise.then(function(response){
-      console.log(response);
-      document.querySelector('.container').innerHTML+= response.data;
-    }).catch(function(error){
-      console.log(error);
-    });
-    document.querySelector('.container').innerHTML+='loading';
+    feet.url=urls[0]
+    feet.change = function(){
+      if(feet.url==urls[0]){
+        feet.url=urls[1];
+      }else{
+        feet.url=urls[0];
+      }
+        feet.load();
+    };
+    feet.load = function(){
+      $timeout( () => {
+      var promise = getGifService.getGif(feet.url);
+      promise.then(function(response){
+        // console.log(response);
+        feet.header=response.config.headers;
+        feet.status=response.status;
+        feet.data=response.data;
+        document.querySelector('.container').innerHTML=response.data;
+      }).catch(function(error){
+        console.log(error);
+      });
+    },1000);
+      document.querySelector('.container').innerHTML='Loading...';
   }
+    feet.load();
+}
 
-  function getGifService($http,haatoUrl,hololiveEveryone){
+  function getGifService($http){
     var service = this;
-    service.getHaato = function(){
-      return $http.get(haatoUrl);
+    service.getGif = function(url){
+      return $http.get(url);
     };
   }
 
