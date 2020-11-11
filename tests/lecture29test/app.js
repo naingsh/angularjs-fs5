10b8ee1d389fb064ace0b526473d4a463dbea6bc
Pine <ns.h32ab@gmail.com>
2020-11-11 12:51:58 +0630

    Tests on promise and directives

--- tests/lecture29test/app.js
+++ tests/lecture29test/app.js
@@ -2,45 +2,80 @@
   'use strict';
     angular.module('app',[])
     .controller('controller',controller)
-    .directive('myDir',myDir);
+    .directive('myDir',myDir)
+    .controller('stopWatch',stopWatch)
+    .directive('myWatch',myWatch)
+    .constant('everyoneUrl',
+    'getGifUrl.html')
+    .directive('myHato',myHato);
 
-    // function myDir() {
-    //   return {
-    //     templateUrl: 'nameChange.html',
-    //     scope:{
-    //       name: '<',
-    //       age: '<'
-    //     },
-    //     controller: nameChangeCtrl,
-    //     controllerAs: 'change',
-    //     bindToController: true
-    //   };
-    // }
-    //
-    // function nameChangeCtrl() {
-    //   var change = this;
-    //   change.change = function(){
-    //     change.name='Naomi';
-    //   }
-    // }
+    function myHato(){
+      return {
+        template: '<img src="../single-html/media/hatoPanik.gif">'
+      }
+    }
+
+    function myWatch(){
+      return{
+        templateUrl: 'watch.html',
+        scope:{
+          start: '<',
+          seconds: '<',
+          stop: '<attr',
+          reset: '<resetter'
+        },
+        controller: gifInsert,
+        controllerAs: 'gif',
+        bindToController: false
+      }
+    }
+
+    function gifInsert($http,everyoneUrl) {
+      var promise = this;
+      promise.getGif = function(){
+      var gif = get(everyoneUrl);
+      function get(url){
+        return $http.get(url);
+      }
+        gif.then(
+          function(response){
+            document.querySelector('.gif').innerHTML= response.data;
+          }
+        ).catch(function(error){
+          console.log(error);
+        });
+      }
+    }
+
+    function stopWatch($interval){
+      var watch = this;
+      watch.seconds=0
+      watch.counter = function(){
+        watch.seconds++;
+      };
+      var startCount;
+    watch.start = function(){
+  startCount =  $interval(watch.counter,100);
+  }
+  watch.reset = function(){
+    watch.seconds=0;
+    $interval.cancel(startCount);
+  }
+      watch.stop = function(){
+        $interval.cancel(startCount);
+      }
+    }
+
     function myDir(){
         return {
           templateUrl: 'nameChange.html',
           scope:{
-            // name: '<',
-            // age:  '<',
             person: '='
          }
-        // controller: newController,
-        // controllerAs: 'person',
-        // bindToController: true
         };
     }
     function newController() {
       var person = this;
-      // person.changeName = function(){
-      //   person.name='naomi';
-      // }
     }
 
     function controller() {
